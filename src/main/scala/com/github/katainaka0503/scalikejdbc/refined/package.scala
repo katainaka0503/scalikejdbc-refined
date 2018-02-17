216a2e8a3993cf691fd694527026829f0059929a
katainaka0503 <katainaka0503@gmail.com>
2018-02-17 23:34:01 +0900

    Reformatted

--- src/main/scala/com/github/katainaka0503/scalikejdbc/refined/package.scala
+++ src/main/scala/com/github/katainaka0503/scalikejdbc/refined/package.scala
@@ -1,11 +1,10 @@
 package com.github.katainaka0503.scalikejdbc
 
-import eu.timepit.refined.api.{ Refined, Validate }
+import eu.timepit.refined.api.{Refined, Validate}
 import scalikejdbc.TypeBinder
 
 package object refined {
-  implicit def refinedTypeBinder[T, P](
-      implicit validate: Validate[T, P],
-               based: TypeBinder[T]): TypeBinder[Refined[T, P]] =
+  implicit def refinedTypeBinder[T, P](implicit validate: Validate[T, P],
+                                       based: TypeBinder[T]): TypeBinder[Refined[T, P]] =
     based.map(Refined.unsafeApply)
 }
